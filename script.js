feed1d7712918dbafaa5d24406909a2c1e0efc00
Sean Gombart <skgombart@gmail.com>
2022-12-14 10:21:25 -0800

    Set up html boilerplate, create basic math operator functions and operate function.

---
+++ script.js
@@ -0,0 +1,20 @@
+// Math operation functions
+function add(x, y) {
+    return x + y;
+};
+
+function subtract(x, y) {
+    return x - y;
+};
+
+function multiply(x, y) {
+    return x * y;
+};
+
+function divide(x, y) {
+    return x / y;
+};
+
+function operate(func, x, y){
+    return func(x, y);
+};
