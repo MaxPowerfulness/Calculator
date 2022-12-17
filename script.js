bb4fbd3cbf7671c27e9d6f7d6da82efb3f76c8f9
Sean Gombart <skgombart@gmail.com>
2022-12-16 23:40:25 -0800

    Fix calculator options, add ability to display inputed numbers to the lower display.

--- script.js
+++ script.js
@@ -18,3 +18,58 @@ function divide(x, y) {
 function operate(func, x, y){
     return func(x, y);
 };
+
+// Global varibales
+const lowerDisp = document.querySelector('.lower');
+
+
+// Adds numbers to the display when button is clicked
+nums = document.querySelectorAll("#num");
+console.log('nums:', nums);
+nums.forEach(num => num.addEventListener('click', numToLowDisp));
+
+/*
+Updates the lower display to reflect the button that was pressed by the user.
+*/
+function numToLowDisp(event) {
+    const className = buttonPressed(event);
+    console.log(className);
+    switch (className) {
+        case 'zero':
+            lowerDisp.insertAdjacentHTML("beforeend", '0');
+            break;
+        case 'one':
+            lowerDisp.insertAdjacentHTML("beforeend", '1');
+            break;
+        case 'two':
+            lowerDisp.insertAdjacentHTML("beforeend", '2');
+            break;
+        case 'three':
+            lowerDisp.insertAdjacentHTML("beforeend", '3');
+            break;
+        case 'four':
+            lowerDisp.insertAdjacentHTML("beforeend", '4');
+            break;
+        case 'five':
+            lowerDisp.insertAdjacentHTML("beforeend", '5');
+            break;
+        case 'six':
+            lowerDisp.insertAdjacentHTML("beforeend", '6');
+            break;
+        case 'seven':
+            lowerDisp.insertAdjacentHTML("beforeend", '7');
+            break;
+        case 'eight':
+            lowerDisp.insertAdjacentHTML("beforeend", '8');
+            break;
+        case 'nine':
+            lowerDisp.insertAdjacentHTML("beforeend", '9');
+            break;
+    }
+};
+
+// Get class of Clicked Element
+const buttonPressed = e => {
+    return e.target.className;  
+  }
+
